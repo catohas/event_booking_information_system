a6a6277a9478e0ff539c20680f2cc2e64466cd6b
catohas <petr.hron04@gmail.com>
2025-10-16 22:39:01 +0200

    Add Event model relations

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
@@ -13,6 +14,16 @@ class Event extends Model
     public function hall(): BelongsTo
     {
         return $this->belongsTo(Hall::class);
+    }
+
+    public function showing(): BelongsTo
+    {
+        return $this->belongsTo(Showing::class);
+    }
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
     }
 
     protected function casts(): array
