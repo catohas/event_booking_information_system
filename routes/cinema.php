4e0588a13f7e7561e49181bb14d0e5cde2a87b90
catohas <petr.hron04@gmail.com>
2025-10-18 20:07:52 +0200

    Add reservations to available routes

--- routes/cinema.php
+++ routes/cinema.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\HallController;
 use App\Http\Controllers\ShowingController;
+use App\Http\Controllers\ReservationController;
 
 Route::get('/events', [EventController::class, 'index']);
 Route::get('/halls', [HallController::class, 'index']);
 Route::get('/showings', [ShowingController::class, 'index']);
+Route::get('/reservations', [ReservationController::class, 'index']);
