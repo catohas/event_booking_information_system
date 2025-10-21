7e596ef308bb6559dfe8622d451eef080f3a1557
catohas <petr.hron04@gmail.com>
2025-10-22 00:51:19 +0200

    Add text for when there are no events

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -49,7 +49,7 @@ export default function Welcome() {
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
                     <main className="flex w-full max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
                         <div className="flex-1 rounded-xl bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
-                            <EventList events={events} />
+                            {events.length ? <EventList events={events} /> : "Zatím zde nejsou vystaveny žádné události"}
                         </div>
                     </main>
                 </div>
