b8348617d25994955889cedcabe96b33e1d92f8a
catohas <petr.hron04@gmail.com>
2025-10-23 17:17:42 +0200

    Register routes for managing users

--- routes/cinema.php
+++ routes/cinema.php
@@ -5,10 +5,21 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\HallController;
 use App\Http\Controllers\ShowingController;
 use App\Http\Controllers\ReservationController;
+use \App\Http\Controllers\Auth\RegisteredUserController;
 
 Route::get('/events', [EventController::class, 'index']);
 Route::get('/halls', [HallController::class, 'index']);
 Route::get('/showings', [ShowingController::class, 'index']);
 Route::get('/reservations', [ReservationController::class, 'index']);
+
 Route::get('/events/{event}/reservations', [ReservationController::class, 'eventReservations'])
     ->name('events.reservations');
+
+Route::get('users', [RegisteredUserController::class, 'index'])
+    ->name('users');
+
+Route::patch('users/{user}', [RegisteredUserController::class, 'update'])
+    ->name('users.update');
+
+Route::delete('users/{user}', [RegisteredUserController::class, 'destroy'])
+    ->name('users.destroy');
