0e0d398c2e9d743f680637ecac5d0ccd2d8d60e3
catohas <petr.hron04@gmail.com>
2025-10-23 17:21:46 +0200

    Update home page

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,7 +1,15 @@
-import { dashboard, login, register } from '@/routes';
-import { type SharedData, type Event } from '@/types';
+import { login, register } from '@/routes';
+import { type SharedData, type Event, type BreadcrumbItem } from '@/types';
 import { Head, Link, usePage } from '@inertiajs/react';
 import EventList from '@/pages/EventList';
+import AppLayout from '@/layouts/app-layout';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Welcome',
+        href: '/',
+    },
+];
 
 export default function Welcome() {
     const { props } = usePage<{ events: { data: Event[] } }>();
@@ -10,7 +18,7 @@ export default function Welcome() {
     const { auth } = usePage<SharedData>().props;
 
     return (
-        <>
+        <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Welcome">
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link
@@ -21,26 +29,19 @@ export default function Welcome() {
             <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
                 <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
                     <nav className="flex items-center justify-end gap-4">
-                        {auth.user ? (
-                            <Link
-                                href={dashboard()}
-                                className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                            >
-                                Dashboard
-                            </Link>
-                        ) : (
+                        {!auth.user && (
                             <>
                                 <Link
                                     href={login()}
                                     className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
                                 >
-                                    Log in
+                                    Přihlášení
                                 </Link>
                                 <Link
                                     href={register()}
                                     className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                                 >
-                                    Register
+                                    Registrace
                                 </Link>
                             </>
                         )}
@@ -55,6 +56,6 @@ export default function Welcome() {
                 </div>
                 <div className="hidden h-14.5 lg:block"></div>
             </div>
-        </>
+        </AppLayout>
     );
 }
